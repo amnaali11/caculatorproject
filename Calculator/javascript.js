1cc98ef5f387625e9528bad4bfd4255a5ba91c5c
amna <85313057+amnaali11@users.noreply.github.com>
2022-04-24 23:31:53 +0200

    Add files via upload

---
+++ Calculator/javascript.js
@@ -0,0 +1,169 @@
+let buttons=Array.from(document.querySelectorAll(".btn"));
+let operations=Array.from(document.querySelectorAll(".o"));
+let display=document.querySelector(".container");
+let prevnum="";
+let newnum="";
+let oper="";
+let res=0;
+buttons.map(function add(btn){
+
+
+btn.addEventListener("click",(e)=>{
+switch(e.target.innerHTML){
+case "Reset":{
+setTimeout(() => {
+    location.reload()
+}, 200);
+}
+break;
+case "Delete":{
+display.innerHTML=display.innerHTML.slice(0,-1);
+prevnum=display.innerHTML;
+
+
+
+}
+break;
+case ".":{
+if(display.innerHTML.includes("."))
+return 0;
+
+}
+
+
+default:{display.innerHTML+=e.target.innerText;
+    if(oper=="")
+prevnum+=e.target.innerHTML;
+else{
+    newnum+=e.target.innerHTML;
+}
+
+console.log("pre",prevnum);
+console.log("new",newnum);
+
+}
+
+
+
+}
+
+})
+
+
+
+});
+operations.map((o)=>{
+o.addEventListener("click",(e)=>{
+    if(oper=="")
+    
+{    oper=e.target.innerHTML;
+}
+
+display.innerHTML+=e.target.innerHTML;
+if(newnum!=""){
+
+    
+switch(oper)
+{
+case "+":{
+res= Number(prevnum)+Number(newnum);
+display.innerHTML=res;
+prevnum=res;
+newnum="";
+oper="";
+
+}
+break;
+case "-":{
+    res= Number(prevnum)-Number(newnum);
+    display.innerHTML=res;
+    prevnum=res;
+    newnum="";
+    oper="";
+
+}
+break;
+case "X":
+{    res= Number(prevnum)*Number(newnum);
+    display.innerHTML=res;
+
+    prevnum=res;
+    newnum="";
+    oper="";
+
+
+}
+break;
+case "/":
+{
+res= Number(prevnum)/Number(newnum);
+display.innerHTML=res;
+prevnum=res;
+newnum="";
+oper="";
+
+
+}
+}
+}
+
+
+
+
+})
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+ function equal(){
+
+
+switch(oper)
+{
+case "+":{
+res= Number(prevnum)+Number(newnum);
+display.innerHTML=res;
+prevnum=res;
+newnum="";
+
+}
+break;
+case "-":{
+    res= Number(prevnum)-Number(newnum);
+    display.innerHTML=res;
+    prevnum=res;
+    newnum="";
+
+}
+break;
+case "X":
+{    res= Number(prevnum)*Number(newnum);
+    display.innerHTML=res;
+
+    prevnum=res;
+    newnum="";
+
+}
+break;
+case "/":
+{
+res= Number(prevnum)/Number(newnum);
+display.innerHTML=res;
+prevnum=res;
+newnum="";
+
+}
+}
+
+
+};
