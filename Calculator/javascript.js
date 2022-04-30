53ef3a1fd9a825d2c5cbcba3452f53b03caf58ae
amna <85313057+amnaali11@users.noreply.github.com>
2022-04-30 04:33:42 +0200

    Update javascript.js

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -24,13 +24,7 @@ prevnum=display.innerHTML;
 
 }
 break;
-case ".":{
-if(prevnum.includes(".")&&newnum=="")
-return 0;
-else if(newnum.includes("."))
-return 0;
 
-}
 
 
 default:{display.innerHTML+=e.target.innerText;
